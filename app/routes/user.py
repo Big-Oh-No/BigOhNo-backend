5877cd0c1405243203b886589f1f0ca0cafe2a99
Anand <clef@Anands-MacBook-Pro.local>
2024-03-09 19:06:09 -0800

    added signup route with unit testing

--- app/routes/user.py
+++ app/routes/user.py
@@ -5,6 +5,7 @@ from ..utils.db import get_db
 from ..utils.utils import hash
 from ..models import user_model
 from ..schemas import user_schema
+import re
 
 router = APIRouter(
     prefix="/user",
@@ -17,7 +18,7 @@ router = APIRouter(
     status_code=200,
 )
 async def login(
-    user: user_schema.User,
+    user: user_schema.UserSignIn,
     db: Session = Depends(get_db),    
 ):
     """Logs a user in"""
@@ -31,3 +32,72 @@ async def login(
         )
     
     return {"message" : "User Authenticated"}
+
+
+
+@router.post(
+    "/signup",
+    status_code=201
+)
+async def signup(
+    info: user_schema.UserSignUp,
+    db: Session = Depends(get_db)
+):
+    """ Create user account """
+
+    if len(info.first_name) == 0 or not (all(char.isalpha() or char == "'" for char in info.first_name)):
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Invalid First Name"
+        )
+    
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    regex = re.compile(pattern)
+    if not regex.match(info.email):
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Invalid Email Address"
+        )
+
+    if len(info.password) < 6:
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Password must be at least 6 characters long"
+        )
+    
+    user_exist = db.query(user_model.User).filter(user_model.User.email == info.email).first()
+    if user_exist:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Account with this email already exists"
+        )
+
+    user = user_model.User(
+        first_name = info.first_name,
+        last_name = info.last_name,
+        email = info.email,
+        password = hash(info.password), # password
+        role = info.role,
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    if user.role == user_model.Role.admin:
+        admin = user_model.Admin(user_id = user.id)
+        db.add(admin)
+        db.commit()
+        db.refresh(admin)
+    elif user.role == user_model.Role.teacher:
+        teacher = user_model.Teacher(user_id = user.id)
+        db.add(teacher)
+        db.commit()
+        db.refresh(teacher)
+    else:
+        student = user_model.Student(user_id = user.id)
+        db.add(student)
+        db.commit()
+        db.refresh(student)
+
+    return {"message": "Account created successfully"}
