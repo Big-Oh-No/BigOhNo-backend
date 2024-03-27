2b2a16ef8ef27ab94f8cf3f24b10f9b8d65cd671
Anand <clef@Anands-MacBook-Pro.local>
2024-03-27 02:12:57 -0700

    added role

--- test/course/test_get_one_course.py
+++ test/course/test_get_one_course.py
@@ -7,3 +7,8 @@ def test_get_course_student(authorized_client, test_courses, test_enrollment, te
 def test_get_course_student_when_not_approved(authorized_client, test_courses, test_submission,  test_verified_student_1):
     res = authorized_client.post(f"/course/{test_courses[0].id}",json={'email': 'student1@korse.com', 'password': 'password'})
     assert res.status_code == 401
+
+def test_get_course_student(authorized_client, test_courses, test_enrollment, test_submission,  test_verified_teacher):
+    res = authorized_client.post(f"/course/{test_courses[0].id}",json={'email': 'teacher@korse.com', 'password': 'password'})
+    assert res.status_code == 200
+    assert res.json()["responses"] == ""
